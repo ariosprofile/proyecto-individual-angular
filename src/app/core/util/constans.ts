4cd18380dad6323d693959b7f0871ab48f848ba5
ariosprofile <arios@profile.es>
2024-05-24 10:53:22 +0200

    Error fixes and constants implementation.

---
+++ src/app/core/util/constans.ts
@@ -0,0 +1,4 @@
+export const CONSTANS = {
+    erroMessageIdentification: 'Correo electrónico o contraseña no válidos. Inténtalo de nuevo.',
+    errorLogin: 'Se produjo un error al iniciar sesión. Vuelva a intentarlo más tarde.'
+}
